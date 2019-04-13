c6c85c4b0fe174654d1042ab55506db85cf4057a
Glitch (hello-express) <none>
2019-04-13 06:35:00 +0000

    😘🚧 Checkpoint ./src/commands/cacapost.js:894797/1969 ./watch.json:894797/6 ./src/commands/help.js:894797/134

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,5 +2,7 @@ module.exports.run = (client, message, args) => {
   message.reply(`comandos de mierdaXD: 
 losmascrakcs - te dise kienes son los mas crakcs lol
 ping - te dise el ping del bot
-help - lo q estas biendo :rolling_eyes:`) //suquiya XD
+help - lo q estas biendo :rolling_eyes:
+comandos masomenos bien echosXD:
+cacapost - obten shitpost de calidad. (actualmente contamos con 41mems XD)`) //suquiya XD
 }
