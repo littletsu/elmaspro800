30b25f94d2fe18c186ef69f4484d6513d74ac4a5
Glitch (hello-express) <none>
2019-04-13 19:32:39 +0000

    🎀🌔 Checkpoint ./src/commands/cacapost.js:894797/86 ./.glitch-assets:894797/403

--- src/commands/cacapost.js
+++ src/commands/cacapost.js
@@ -40,7 +40,8 @@ https://cdn.discordapp.com/attachments/528803791462727680/559468785325834249/FB_
 https://cdn.discordapp.com/attachments/528803791462727680/559468784729980959/FB_IMG_1553454949473.jpg
 https://cdn.discordapp.com/attachments/534223169104510987/566510147610869760/cuandoalguien_este_en_mi_chat.png
 https://cdn.discordapp.com/attachments/534223169104510987/566512006639910912/jaja_alchile.jpg
-https://cdn.discordapp.com/attachments/534223169104510987/566512948051181595/SPOILER_gnome_people.png`
+https://cdn.discordapp.com/attachments/534223169104510987/566512948051181595/SPOILER_gnome_people.png
+https://cdn.glitch.com/207b31df-ca8c-43d4-aa1d-b9a9de73545a%2Fimage.png?1555183396101`
 //let shitpost = ['https://cdn.discordapp.com/attachments/495657784893898772/558014678664478721/image0-2.jpg','https://cdn.discordapp.com/attachments/404038475986370563/558389561936314399/asistencia.jpg','https://cdn.discordapp.com/attachments/519033872894394383/558469923538731038/IMG_20190321_225629.jpg','https://cdn.discordapp.com/attachments/519033872894394383/559015837806231563/FB_IMG_1550634338616.png','https://cdn.discordapp.com/attachments/519033872894394383/559130072183996417/FB_IMG_15533772531797440.jpg','https://cdn.discordapp.com/attachments/519033872894394383/560301839099363338/IMG-20190324-WA0022.jpg','https://cdn.discordapp.com/attachments/519033872894394383/560826462991679488/FB_IMG_1553548632256.jpg','https://cdn.discordapp.com/attachments/519033872894394383/560826462991679488/FB_IMG_1553548632256.jpg','https://cdn.discordapp.com/attachments/519033872894394383/560973013516943391/IMG-20190305-WA0007.jpg','https://cdn.discordapp.com/attachments/519033872894394383/560973227426316313/37241345_932705040233857_7260184404733984768_n.jpg']
 module.exports.run = (client, message, args) => {
   
