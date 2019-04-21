796b886bbb57b7a0190183b9d412eb069a06471c
Glitch (hello-express) <none>
2019-04-21 19:30:26 +0000

    🎠🏣 Checkpoint ./src/commands/ahorcado.js:894797/1462 ./package.json:894797/1350

--- src/commands/ahorcado.js
+++ src/commands/ahorcado.js
@@ -1,7 +1,13 @@
 module.exports.run = (client, message, args) => {
   let palabra = null;
   
-  if(!message.mentions.users.first()) message.reply("Necesitas mencionar a alguien con quien jugar!");
+  if(!message.mentions.users.first()) return message.reply("Necesitas mencionar a alguien con quien jugar!");
+  
+  let jugador = message.mentions.users.first();
+  
+  if(jugador.bot) return message.reply("No puedes jugar con bots.");
+  if(jugador == message.author) return message.reply("Menciona a alguien mas!");
+  
   message.reply("Envia la palabra que quisieras usar para este juego a mis mensajes directos! (tienes 15 segundos!)").then((msg) => {
     message.author.send("Envia la palabra!").catch(() => {
       msg.edit("Porfavor activa tus mensajes directos para que me puedas enviar la palabra!")
@@ -11,14 +17,21 @@ module.exports.run = (client, message, args) => {
       let hasCollected = false;
       
       
-      DMCollector.once("collect", m => {
-        palabra = m.content;
+      DMCollector.once("collect", CollectedMessage => {
+        palabra = CollectedMessage.content;
         hasCollected = true;
-        console.log(palabra)
+        message.channel.send(`La palabra se ha elegido. <@${jugador.id}>, quieres jugar al ahorcado con <@${message.author.id}>? (No reacciones hasta que se pongan todas las reacciones!)`).then((PromptMessage) => {
+          PromptMessage.react("✅").then(() => PromptMessage.react("❌").then(() => {
+            let filter = (reaction, user) => (reaction.emoji.name === '❌' || reaction.emoji.name === '✅') && user.id === jugador.id;
+            let collector = message.createReactionCollector(filter, { time: 60e3 });
+            collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
+            collector.on('end', collected => console.log(`Collected ${collected.size} items`));
+          }));
+        })
       })
       
       DMCollector.once("end", () => {
-        if(!hasCollected) 
+        if(!hasCollected) return message.author.send("Se acabo el tiempo.");
       })
     })
   })
