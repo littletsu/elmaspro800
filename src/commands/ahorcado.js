8e1addc61da396cd9417612d6a14ca73cf9bf760
Glitch (hello-express) <none>
2019-04-21 22:03:30 +0000

    🌕🎃 Checkpoint ./src/commands/ahorcado.js:894797/14548

--- src/commands/ahorcado.js
+++ src/commands/ahorcado.js
@@ -8,7 +8,7 @@ const AhorcadoStates = [`
 |             / \\
 |
 `,
-`
+    `
 |----------------
 |              |
 |              0
@@ -16,8 +16,8 @@ const AhorcadoStates = [`
 |              |
 |             / 
 |
-`, 
-`
+`,
+    `
 |----------------
 |              |
 |              0
@@ -25,7 +25,7 @@ const AhorcadoStates = [`
 |              |
 |              
 |`,
-`
+    `
 |----------------
 |              |
 |              0
@@ -33,7 +33,7 @@ const AhorcadoStates = [`
 |              
 |             
 |`,
-`
+    `
 |----------------
 |              |
 |              0
@@ -41,7 +41,7 @@ const AhorcadoStates = [`
 |              
 |             
 |`,
-`
+    `
 |----------------
 |              |
 |              0
@@ -49,7 +49,7 @@ const AhorcadoStates = [`
 |              
 |             
 |`,
-`
+    `
 |----------------
 |              |
 |              0
@@ -57,123 +57,138 @@ const AhorcadoStates = [`
 |              
 |             
 |`,
-`
+    `
 |----------------
 |              |
 |              
 | 
 |              
 |             
-|`]
+|`
+]
 
 
 module.exports.run = (client, message, args) => {
-  let palabra = null;
-  if(jugando.has(message.author.id)) return message.reply("Ya estas jugando con alguien mas, o estas en una decision.");
-  if(!message.mentions.users.first()) return message.reply("Necesitas mencionar a alguien con quien jugar!");
-  
-  let jugador = message.mentions.users.first();
-  
-  if(jugador.bot) return message.reply("No puedes jugar con bots.");
-  if(jugador == message.author) return message.reply("Menciona a alguien mas!");
-  jugando.add(message.author.id);
-  jugando.add(jugador.id);
-  
-  message.reply("Envia la palabra que quisieras usar para este juego a mis mensajes directos! (tienes 15 segundos!)").then((msg) => {
-    message.author.send("Envia la palabra!").catch(() => {
-      msg.edit("Porfavor activa tus mensajes directos para que me puedas enviar la palabra!")
-      jugando.delete(message.author.id);
-      jugando.delete(jugador.id);
-    }).then((m) => {
-      let filter = mes => mes.author.id == message.author.id;
-      let DMCollector = m.channel.createMessageCollector(filter, {time: 15e3})
-      let hasCollected = false;
-      
-      
-      DMCollector.once("collect", CollectedMessage => {
-        palabra = CollectedMessage.content.toLowerCase();
-        hasCollected = true;
-        message.channel.send(`La palabra se ha elegido. <@${jugador.id}>, quieres jugar al ahorcado con <@${message.author.id}>? (No reacciones hasta que se pongan todas las reacciones!)`).then((PromptMessage) => {
-          PromptMessage.react("✅").then(() => PromptMessage.react("❌").then(() => {
-            PromptMessage.edit(`La palabra se ha elegido. <@${jugador.id}>, quieres jugar al ahorcado con <@${message.author.id}>?`);
-            let ReactionFilter = (reaction, user) => user.id === jugador.id;
-            let PromptCollector = PromptMessage.createReactionCollector(ReactionFilter, { time: 15e3});
-            let hasCollectedPrompt = false;
+    let palabra = null;
+    if (jugando.has(message.author.id)) return message.reply("Ya estas jugando con alguien mas, o estas en una decision.");
+    if (!message.mentions.users.first()) return message.reply("Necesitas mencionar a alguien con quien jugar!");
 
-            PromptCollector.once("collect", (reaction) => {
-              hasCollectedPrompt = true;
-              console.log(reaction)
-              switch(reaction.emoji.name) {
-                case "✅":
-                  let palabranoseq = palabra.split('');
-                  let unrevealedWord = "_".repeat(palabranoseq.length).split('');
-                  var gameState = 7;
-                  PromptMessage.delete()
-                  msg.delete()
-                  message.reply("Empezando el juego!").then(AhorcadoMessage => {
-                    AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
+    let jugador = message.mentions.users.first();
+
+    if (jugador.bot) return message.reply("No puedes jugar con bots.");
+    if (jugador == message.author) return message.reply("Menciona a alguien mas!");
+    jugando.add(message.author.id);
+    jugando.add(jugador.id);
+
+    message.reply("Envia la palabra que quisieras usar para este juego a mis mensajes directos! (tienes 15 segundos!)").then((msg) => {
+        message.author.send("Envia la palabra!").catch(() => {
+            msg.edit("Porfavor activa tus mensajes directos para que me puedas enviar la palabra!")
+            jugando.delete(message.author.id);
+            jugando.delete(jugador.id);
+        }).then((m) => {
+            let filter = mes => mes.author.id == message.author.id;
+            let DMCollector = m.channel.createMessageCollector(filter, {
+                time: 15e3
+            })
+            let hasCollected = false;
+
+
+            DMCollector.once("collect", CollectedMessage => {
+                palabra = CollectedMessage.content.toLowerCase();
+                hasCollected = true;
+                message.channel.send(`La palabra se ha elegido. <@${jugador.id}>, quieres jugar al ahorcado con <@${message.author.id}>? (No reacciones hasta que se pongan todas las reacciones!)`).then((PromptMessage) => {
+                    PromptMessage.react("✅").then(() => PromptMessage.react("❌").then(() => {
+                        PromptMessage.edit(`La palabra se ha elegido. <@${jugador.id}>, quieres jugar al ahorcado con <@${message.author.id}>?`);
+                        let ReactionFilter = (reaction, user) => user.id === jugador.id;
+                        let PromptCollector = PromptMessage.createReactionCollector(ReactionFilter, {
+                            time: 15e3
+                        });
+                        let hasCollectedPrompt = false;
+
+                        PromptCollector.once("collect", (reaction) => {
+                            hasCollectedPrompt = true;
+                            console.log(reaction)
+                            switch (reaction.emoji.name) {
+                                case "✅":
+                                    let palabranoseq = palabra.split('');
+                                    let unrevealedWord = "_".repeat(palabranoseq.length).split('');
+                                    var gameState = 7;
+                                    PromptMessage.delete()
+                                    msg.delete()
+                                    message.reply("Empezando el juego!").then(AhorcadoMessage => {
+                                        AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
 
 Palabra: ${unrevealedWord.join(' ')}\`\`\``).then(() => {
-                      const PalabrasFilter = m => m.author == jugador;
-                      let hasWin = false;
-                      
-                      const PalabrasCollector = message.channel.createMessageCollector(PalabrasFilter, { time: 60e3 });
-                      PalabrasCollector.on('collect', m => {
-                        if(palabranoseq.includes(m.content)) {
-                          palabranoseq.forEach((char, i) => {
-                            if(char == m.content) unrevealedWord[i] = m.content
-                            AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
+                                            const PalabrasFilter = m => m.author == jugador;
+                                            let hasWin = false;
+                                            let hasLost = false;
+                                            const PalabrasCollector = message.channel.createMessageCollector(PalabrasFilter, {
+                                                time: 60e3
+                                            });
+                                            PalabrasCollector.on('collect', m => {
+                                                if (!(hasWin ^ hasLost)) {
+                                                    if (palabranoseq.includes(m.content)) {
+                                                        palabranoseq.forEach((char, i) => {
+                                                            if (char == m.content) unrevealedWord[i] = m.content
+                                                            AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
 
 Palabra: ${unrevealedWord.join(' ')}\`\`\``);
-                           // console.log(palabranoseq)
-                          })
-                        } else {
-                          gameState--
-                          AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
+                                                            // console.log(palabranoseq)
+                                                        })
+                                                    } else {
+                                                        gameState--
+                                                        AhorcadoMessage.edit(`\`\`\`${AhorcadoStates[gameState]}
 
 Palabra: ${unrevealedWord.join(' ')}\`\`\``)
-                        }
-                        
-                        console.log(unrevealedWord)
-                        m.delete()
-                      });
-                      PalabrasCollector.on('end', collected => {
-                        message.reply("Tiempo de la partida se ha acabado.")
-                        jugando.delete(message.author.id);
-                        jugando.delete(jugador.id);
-                      });
-                    })
-                  });
-                  break;
-                case "❌":
-                  message.reply(":(");
-                  jugando.delete(message.author.id);
-                  jugando.delete(jugador.id);
-                  break;
-              }
+                                                        if (gameState <= 0) {
+                                                            AhorcadoMessage.edit(`<@${message.author.id}> ha ganado! La palabra era: ${palabra}`)
+                                                        }
+                                                    }
+
+                                                    console.log(unrevealedWord)
+                                                    m.delete()
+                                                }
+                                            });
+
+                                            PalabrasCollector.on('end', collected => {
+                                              if(!(hasWin ^ hasLost)) {
+                                                message.reply("Tiempo de la partida se ha acabado.")
+                                                jugando.delete(message.author.id);
+                                                jugando.delete(jugador.id);
+                                              }
+                                            });
+                                        })
+                                    });
+                                    break;
+                                case "❌":
+                                    message.reply(":(");
+                                    jugando.delete(message.author.id);
+                                    jugando.delete(jugador.id);
+                                    break;
+                            }
+                        })
+
+                        PromptCollector.once("end", () => {
+                            if (!hasCollectedPrompt) {
+                                message.reply("El usuario no respondio a tiempo.");
+                                jugando.delete(message.author.id);
+                                jugando.delete(jugador.id);
+                            };
+
+                        })
+
+                    }));
+                })
             })
-            
-            PromptCollector.once("end", () => {
-              if(!hasCollectedPrompt) {
-                message.reply("El usuario no respondio a tiempo.");
-                jugando.delete(message.author.id);
-                jugando.delete(jugador.id);
-              };
-              
+
+            DMCollector.once("end", () => {
+                if (!hasCollected) {
+                    jugando.delete(message.author.id);
+                    jugando.delete(jugador.id);
+                    message.channel.send("Se acabo el tiempo.");
+                    return message.author.send("Se acabo el tiempo.");
+                }
             })
-            
-          }));
         })
-      })
-      
-      DMCollector.once("end", () => {
-        if(!hasCollected) {
-          jugando.delete(message.author.id);
-          jugando.delete(jugador.id);
-          message.channel.send("Se acabo el tiempo.");
-          return message.author.send("Se acabo el tiempo.");
-        }
-      })
     })
-  })
 }
