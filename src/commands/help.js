2c2e853494f4641e3b5fb931585d61a8dd4198fa
Glitch (hello-express) <none>
2019-04-22 02:30:44 +0000

    💗💁 Checkpoint ./src/commands/help.js:894797/137

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,5 +4,7 @@ losmascrakcs - te dise kienes son los mas crakcs lol
 ping - te dise el ping del bot
 help - lo q estas biendo :rolling_eyes:
 **comandos masomenos bien echosXD**:
-cacapost - obten shitpost de calidad. (actualmente contamos con 41mems XD)`) //suquiya XD
+cacapost - obten shitpost de calidad. (actualmente contamos con 41mems XD)
+**comandos dep inche calidad puta msadre**:
+ahorcado [usuario] - juega al aorcado con alguien,l ol`) //suquiya XD
 }
