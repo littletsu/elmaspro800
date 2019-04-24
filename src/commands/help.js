ba38f334e101535fb4d022809ea201e00efec305
Glitch (hello-express) <none>
2019-04-24 18:44:10 +0000

    🎾🐋 Checkpoint ./src/commands/top.js:894797/363 ./src/commands/help.js:894797/128

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,7 +5,10 @@ ping - te dise el ping del bot
 help - lo q estas biendo :rolling_eyes:
 **comandos masomenos bien echosXD**:
 cacapost - obten shitpost de calidad. (actualmente contamos con 41mems XD)
+**comandos de typeraser**:
 typeracer - juega una version muy mala de typeracer con tus amigos (si es q tienes lol)
+wins - ve tus wins en typeracer
+top - ve el top de lagente sin vida q ha ganado mas partidas
 **comandos dep inche calidad puta msadre**:
 ahorcado [usuario] - juega al aorcado con alguien,l ol`) //suquiya XD
 }
--- src/commands/top.js
+++ src/commands/top.js
@@ -1,3 +1,12 @@
-module.exports.run = (client, messsage, args) => {
-  
+module.exports.run = (client, message, args) => {
+   client.TypeRacerDB.datos().then(datos => {
+   var datosI = []
+   for(var dato in datos) {
+     datosI.push([dato, datos[dato].wins])
+    }
+    var sortedUsers = datosI.sort(function(a, b) {
+            return b[1] - a[1]
+     });
+     message.channel.send(sortedUsers.map((user, i) => `${i+1}. ${client.users.get(user[0]).tag} - ${user[1]} wins`).join('\n'))
+})
 }
