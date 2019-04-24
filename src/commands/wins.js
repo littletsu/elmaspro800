89886ee4863520ff4212741151ee04ac85c321cf
Glitch (hello-express) <none>
2019-04-24 18:11:10 +0000

    🛣🍮 Checkpoint ./src/commands/wins.js:894797/159

--- src/commands/wins.js
+++ src/commands/wins.js
@@ -1,4 +1,3 @@
 module.exports.run = async (client, message, args) => {
-  var id = message.mentions.users.first().id
-  message.reply(`${message.mentions.users.first() ? `Las wins de ${message.mentions.users.first().tag}` : `Tus wins`} en TypeRacer son: ${await client.TypeRacerDB.obtener(id || message.author.id + '.wins')}`)
+  message.reply(`Tus wins en TypeRacer son: ${await client.TypeRacerDB.obtener(message.author.id + '.wins')}`)
 }
