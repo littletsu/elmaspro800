380d94a64544b7b34cf0d3716bfd73dae72d23b6
Glitch (hello-express) <none>
2019-04-25 04:19:54 +0000

    🎟🎮 Checkpoint ./src/commands/wins.js:894797/47

--- src/commands/wins.js
+++ src/commands/wins.js
@@ -1,5 +1,5 @@
 module.exports.run = async (client, message, args) => {
-  client.TypeRacerDB.obtener('266063988209483790').then(datos => {
+  client.TypeRacerDB.obtener(message.author.id).then(datos => {
     let promedio = 0;
     datos.record.map(num => promedio = promedio+num)
     message.reply(`Tus wins en TypeRacer son: ${datos.wins}\nLo que tardas en escribir una oracion en promedio son: ${promedio == 0 ? "Aun no ha sido calculado." : promedio / datos.record.length}s`)
