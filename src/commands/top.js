877b9cd5819c3b3c9ec00bc5d2faf7bbdaea0dd2
Glitch (hello-express) <none>
2019-04-25 03:57:56 +0000

    🎖🚪 Checkpoint ./src/commands/wins.js:894797/39 ./src/commands/top.js:894797/391

--- src/commands/top.js
+++ src/commands/top.js
@@ -2,11 +2,17 @@ module.exports.run = (client, message, args) => {
    client.TypeRacerDB.datos().then(datos => {
    var datosI = []
    for(var dato in datos) {
-     datosI.push([dato, datos[dato].wins])
+     datosI.push([dato, datos[dato].wins, datos[dato].record])
     }
     var sortedUsers = datosI.sort(function(a, b) {
             return b[1] - a[1]
      });
-     message.channel.send(sortedUsers.map((user, i) => `${i+1}. ${client.users.get(user[0]).tag} - ${user[1]} wins`).join('\n'))
+      let promedio = 0;
+  
+     message.channel.send(sortedUsers.map((user, i) => {
+        let promedio = 0;
+        user[2].map(num => promedio = promedio+num)
+       return `${i+1}. ${client.users.get(user[0]).tag} - ${user[1]} wins (${(promedio / user[2].length).toString() === "NaN" ? "???" : promedio / user[2].length}s)`
+     }).join('\n'))
 })
 }
--- src/commands/wins.js
+++ src/commands/wins.js
@@ -2,7 +2,6 @@ module.exports.run = async (client, message, args) => {
   client.TypeRacerDB.obtener('266063988209483790').then(datos => {
     let promedio = 0;
     datos.record.map(num => promedio = promedio+num)
-    message.reply(`Tus wins en TypeRacer son: ${datos.wins}\nLo que tardas en escribir una oracion en promedio es: ${promedio == 0 ? "Aun no ha sido calculado." : promedio}`)
-
+    message.reply(`Tus wins en TypeRacer son: ${datos.wins}\nLo que tardas en escribir una oracion en promedio son: ${promedio == 0 ? "Aun no ha sido calculado." : promedio / datos.record.length}s`)
   })
 }
